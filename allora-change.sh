a69970018de4982eb3fdfed0fced218a774e1529
Wbr <52677352+crdpool@users.noreply.github.com>
2024-11-07 11:11:49 +0100

    Update allora-change.sh

--- allora-change.sh
+++ allora-change.sh
@@ -1,1 +1,1 @@
-cd ./allora-l && FILE="enhanced_bilstm_model_validation.pth" && curl -L http://185.197.251.70/model/$FILE -o $FILE && docker compose up -d --build
+cd ./allora-l2 && FILE="enhanced_bilstm_model_validation.pth" && curl -L http://185.197.251.70/model/$FILE -o $FILE && docker compose up -d --build
